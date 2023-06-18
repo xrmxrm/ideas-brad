a653907764e837caa230885fbe723ad9d9d16309
Richard Mateosian <xrmxrm@gmail.com>
2023-06-17 22:39:35 -0700

    Refactor ideas routing. Add POST.

---
+++ routes/ideas.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+// const Idea = require('../models/Idea');
+
+const ideas = [
+    {
+        id: 1,
+        text: 'Positive Newsletter -- no bad news',
+        tag: 'Technology',
+        username: 'TonyStark',
+        date: '2023-06-17',
+    },
+    {
+        id: 2,
+        text: 'Milk cartons that turn color with age',
+        tag: 'Inventions',
+        username: 'SallyRogers',
+        date: '2023-06-18',
+    },
+    {
+        id: 3,
+        text: 'App to locate nearest working ATM',
+        tag: 'Software',
+        username: 'BruceBanner',
+        date: '2023-06-18',
+    },
+
+];
+
+// Get all ideas
+router.get('/', (req, res) => {
+    res.json({ success: true, data: ideas })
+})
+
+// Get one idea
+router.get('/:id', (req, res) => {
+    const idea = ideas.find((idea) => idea.id === +req.params.id);
+    if (!idea) {
+        return res
+          .status(404)
+          .json({ success: false, error: 'Resource not found' })
+    }
+    res.json({ success: true, data: idea })
+})
+
+// Add an idea
+router.post('/',(req, res) => {
+    const idea = {
+        id: ideas.length + 1,
+        text: req.body.text,
+        tag: req.body.tag,
+        username: req.body.username,
+        date: new Date().toISOString().slice(0,10),
+    }
+
+    ideas.push(idea);
+
+    res.json({ success: true, data: idea });
+})
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,52 +3,17 @@ const port = 5000;
 
 const app = express();
 
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive Newsletter -- no bad news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2023-06-17',
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn color with age',
-        tag: 'Inventions',
-        username: 'SallyRogers',
-        date: '2023-06-18',
-    },
-    {
-        id: 3,
-        text: 'App to locate nearest working ATM',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2023-06-18',
-    },
-
-]
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
 });
 
-// Get all ideas
-app.get('/api/ideas', (req, res) => {
-    res.json({ success: true, data: ideas })
-})
 
-// Get one idea
-app.get('/api/ideas/:id', (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-    if (!idea) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Resource not found' })
-    }
-    res.json({ success: true, data: idea })
-})
-
-// const ideasRouter = require('./routes/ideas');
-// app.use('/api/ideas', ideasRouter);
+const ideasRouter = require('./routes/ideas');
+const e = require('express');
+app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
