88b7f1ac4b5838d4c1a2a0340b6b38b953a295ac
Richard Mateosian <xrmxrm@gmail.com>
2023-06-18 14:56:02 -0700

    Use mongo and Idea schema

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,93 +1,101 @@
 const express = require('express');
 const router = express.Router();
-// const Idea = require('../models/Idea');
-
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive Newsletter -- no bad news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2023-06-17',
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn color with age',
-        tag: 'Inventions',
-        username: 'SallyRogers',
-        date: '2023-06-18',
-    },
-    {
-        id: 3,
-        text: 'App to locate nearest working ATM',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2023-06-18',
-    },
-
-];
+const Idea = require('../models/Idea');
 
 // Get all ideas
-router.get('/', (req, res) => {
-    res.json({ success: true, data: ideas })
-})
+router.get('/', async (req, res) => {
+  try {
+    const ideas = await Idea.find();
+    res.json({ success: true, data: ideas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
+});
 
-// Get one idea
-router.get('/:id', (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-    if (!idea) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Resource not found' })
-    }
-    res.json({ success: true, data: idea })
-})
+// Get single idea
+router.get('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    res.json({ success: true, data: idea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
+});
 
 // Add an idea
-router.post('/',(req, res) => {
-    const idea = {
-        id: ideas.length + 1,
-        text: req.body.text,
-        tag: req.body.tag,
-        username: req.body.username,
-        date: new Date().toISOString().slice(0,10),
-    }
+router.post('/', async (req, res) => {
+  const idea = new Idea({
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+  });
 
-    ideas.push(idea);
-
-    res.json({ success: true, data: idea });
-})
-
-// Delete an idea
-// router.delete('/:id', (req, res) => {
-//     const idea = ideas.find((idea) => idea.id === +req.params.id);
-//     if (!idea) {
-//         return res
-//           .status(404)
-//           .json({ success: false, error: 'Resource not found' })
-//     }
-
-//     const ix = ideas.indexOf(+id);
-//     ideas.splice(ix);
-
-//     res.json({ success: true, data: idea })
-// })
+  try {
+    const savedIdea = await idea.save();
+    res.json({ success: true, data: savedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
+});
 
 // Update idea
-router.put('/:id', (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-    if (!idea) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Resource not found' })
+router.put('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+
+    // Match the usernames
+    if (idea.username === req.body.username) {
+      const updatedIdea = await Idea.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            text: req.body.text,
+            tag: req.body.tag,
+          },
+        },
+        { new: true }
+      );
+      return res.json({ success: true, data: updatedIdea });
     }
 
-idea.text = req.body.text || idea.text;
-idea.tag = req.body.tag || idea.tag;
+    // Usernames do not match
+    res
+      .status(403)
+      .json({
+        success: false,
+        error: 'You are not authorized to update this resource',
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
+});
 
-    res.json({ success: true, data: idea })
-})
+// Delete idea
+router.delete('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
 
+    // Match the usernames
+    if (idea.username === req.body.username) {
+      await Idea.findByIdAndDelete(req.params.id);
+      return res.json({ success: true, data: {} });
+    }
 
+    // Usernames do not match
+    res
+      .status(403)
+      .json({
+        success: false,
+        error: 'You are not authorized to delete this resource',
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
+});
 
 module.exports = router;
